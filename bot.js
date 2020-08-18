4fda3a4af28d7faad68148d53d3d30c869ce483e
Ingo Stramm <ingostramm@gmail.com>
2020-08-18 11:32:37 -0300

    Urban get

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fetch = require('node-fetch');
+const querystring = require('querystring');
 const prefix = '!';
 
 
@@ -21,13 +22,15 @@ client.on('message', async message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-    fetch('https://aws.random.cat/meow').then(response => response.json());
-    
-    if (command === 'cat') {
-        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-    
-        message.channel.send(file);
-    }
+    if (command === 'urban') {
+        if (!args.length) {
+          return message.channel.send('You need to supply a search term!');
+        }
+      
+          const query = querystring.stringify({ term: args.join(' ') });
+      
+        const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
+      }
 
 });
 
