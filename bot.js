b6ebd667fcbcf9880bd61a263ed1d16be32c337a
Ingo Stramm <ingostramm@gmail.com>
2020-08-18 11:34:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,25 +12,32 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-       message.reply('pong');
+        message.reply('pong');
     }
 });
 
 client.on('message', async message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
     if (command === 'urban') {
         if (!args.length) {
-          return message.channel.send('You need to supply a search term!');
+            return message.channel.send('You need to supply a search term!');
         }
-      
-          const query = querystring.stringify({ term: args.join(' ') });
-      
+
+        const query = querystring.stringify({ term: args.join(' ') });
+
         const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
-      }
+
+        if (!list.length) {
+            return message.channel.send(`No results found for **${args.join(' ')}**.`);
+        }
+
+        message.channel.send(list[0].definition);
+
+    }
 
 });
 
