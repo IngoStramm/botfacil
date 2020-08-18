ab9cfe9a0cb9babea2c167bae980a9ffb559fea3
Ingo Stramm <ingostramm@gmail.com>
2020-08-18 11:08:29 -0300

    cat fetch

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fetch = require('node-fetch');
+
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -9,6 +11,22 @@ client.on('message', message => {
     if (message.content === 'ping') {
        message.reply('pong');
     }
+});
+
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    fetch('https://aws.random.cat/meow').then(response => response.json());
+    
+    if (command === 'cat') {
+        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+    
+        message.channel.send(file);
+    }
+
 });
 
 // THIS  MUST  BE  THIS  WAY
